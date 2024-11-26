358c5abfc2dd726371eda3a8cf1968e065b18a0a
bayhaqqyy <21.19498@smkn1adw.sch.id>
2024-11-26 14:01:10 +0700

    update

--- main.go
+++ main.go
@@ -1,23 +1,19 @@
+// main.go
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World v2.1")
-
-	 
-
+    fmt.Fprintf(w, "Hello, Updated World!")  // Update the response
 }
+
 func main() {
-	port := ":8070"
-
-	// Print a message indicating the server is starting
-	fmt.Printf("Starting server on port%s\n", port)
-	http.HandleFunc("/", handler)
-
-	http.ListenAndServe(port, nil)
-	fmt.Printf("Appication stop\n")
+    port := ":8070"
+    fmt.Printf("Starting server on port%s\n", port)
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(port, nil)
+    fmt.Printf("Application stopped\n")
 }
